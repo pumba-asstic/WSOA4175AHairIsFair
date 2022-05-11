2ab6d224660de871a46403548cb7d7a39c048052
pumba-asstic <80956904+pumba-asstic@users.noreply.github.com>
2022-05-11 09:01:13 +0200

    Blah

--- src/index.js
+++ src/index.js
@@ -5,13 +5,23 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//     <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App/>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
